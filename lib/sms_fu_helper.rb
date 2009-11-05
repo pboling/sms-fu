9b2f9002368daf87aa596f89c8849e71e9de16cc
pboling <peter.boling@peterboling.com>
2009-11-05 14:02:34 -0500

    Honor both phrase and include_blank settings, and make both valueless option tags (can have both, neither or either)

--- lib/sms_fu_helper.rb
+++ lib/sms_fu_helper.rb
@@ -12,7 +12,13 @@ module SMSFuHelper
   # - selected => carrier to pre-select
   # - include_blank => Do you want the blank option tag (in ADDITION to the value-less 'phrase' option above)?
   def carrier_select(name = :mobile_carrier, phrase = "Select a Carrier", selected = nil, include_blank = true)
-    options = phrase.nil? ? carrier_collection : include_blank ? [phrase,nil] + carrier_collection : [phrase] + carrier_collection
+    options = phrase.blank? ?
+                include_blank ?
+                        [nil] + carrier_collection :
+                        carrier_collection :
+                include_blank ?
+                        [phrase,nil] + carrier_collection :
+                        [[phrase,nil]] + carrier_collection
     select_tag name, options_for_select(options, selected || phrase)
   end
 
